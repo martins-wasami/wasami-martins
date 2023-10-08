31ac6a992182ea76bd47b3c2b56bf8650a112f3e
Martins Wasami <wasamimartin@gmail.com>
2023-10-08 01:55:02 +0100

    added header and hero section

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import Header from "@/components/Header";
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import HeroSection from "@/components/HeroSection";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,7 +18,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} bg-gray-50 text-gray-950 relative`}>
+      <body
+        className={`${inter.className} bg-gray-50 text-gray-950 relative pt-28 sm:pt-36`}
+      >
         <div className="bg-[#fbe2e3] absolute top-[-6rem] right-[11rem] w-[31.25rem] h-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem ] -z-10"></div>
         <div className="bg-[#dbd7fb] absolute top-[-1rem] left-[-35rem] w-[50rem] h-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] -z-10"></div>
         <Header />
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,9 @@
+import HeroSection from "@/components/HeroSection";
+
 export default function Home() {
-  return <main className=""></main>;
+  return (
+    <main className="flex flex-col items-center px-4">
+      <HeroSection />
+    </main>
+  );
 }
